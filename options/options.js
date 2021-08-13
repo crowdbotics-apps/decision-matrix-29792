7f40ffd08c610b3621c41e4319e5312026d38d58
Crowdbotics <team@crowdbotics.com>
2021-08-13 20:22:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "decision_matrix_29792Identifier",
+  url: "https://decision_matrix_29792Identifier.botics.co",
+  api: "https://decision_matrix_29792Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
